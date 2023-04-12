2e56e7a4aaa580b576fdefc6f6481302b8caafd9
linerec <linerec@naver.com>
2023-04-12 04:12:54 -0400

    golbanc 로고 삽입

--- src/components/banner/SearchBanner.jsx
+++ src/components/banner/SearchBanner.jsx
@@ -4,10 +4,17 @@ function SearchBanner() {
   return (
     <section className="home promo">
       <div className="container">
-        <h1>
-          골프뱅크 <small>Golf Banc</small>
-        </h1>
-        <h2>당신의 골프 라이프를 높이다</h2>
+        <div className="row">
+          <div className="d-none d-sm-block col-md-2 golbanc">
+            <img src="assets/images/sections/searchbanner/golbanc.jpg" />
+          </div>
+          <div className="col-md-10">
+            <h1>
+              골프뱅크 <small>Golf Banc</small>
+            </h1>
+            <h2>당신의 골프 라이프를 높이다</h2>
+          </div>
+        </div>
       </div>
     </section>
   );
